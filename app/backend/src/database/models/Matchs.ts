a23d20afe49840d422744775db9d5e9b756ba8c2
andremorsch <andremorsch37@gmail.com>
2022-03-17 16:48:13 -0300

    cria model de match

---
+++ app/backend/src/database/models/Matchs.ts
@@ -0,0 +1,47 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class Matchs extends Model {
+  id: number;
+
+  homeTeam: number;
+
+  homeTeamGoals: number;
+
+  awayTeam: number;
+
+  awayTeamGoals: number;
+
+  inProgress: boolean;
+}
+
+Matchs.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  homeTeam: DataTypes.INTEGER,
+  homeTeamGoals: DataTypes.INTEGER,
+  awayTeam: DataTypes.INTEGER,
+  awayTeamGoals: DataTypes.INTEGER,
+  inProgress: DataTypes.BOOLEAN,
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'matchs',
+  timestamps: false,
+});
+
+export default Matchs;
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
+// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
+
+// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
