230f34304f7fdb914e1adbe2389e740a03f81bf6
andremorsch <andremorsch37@gmail.com>
2022-03-17 16:47:53 -0300

    cria model de Users

---
+++ app/backend/src/database/models/Users.ts
@@ -0,0 +1,43 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class Users extends Model {
+  id: number;
+
+  username: string;
+
+  role: string;
+
+  email: string;
+
+  password: string;
+}
+
+Users.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  username: DataTypes.STRING,
+  role: DataTypes.STRING,
+  email: DataTypes.STRING,
+  password: DataTypes.STRING,
+}, {
+  underscored: true,
+  sequelize: db,
+  timestamps: false,
+});
+
+export default Users;
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
+// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
+
+// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
