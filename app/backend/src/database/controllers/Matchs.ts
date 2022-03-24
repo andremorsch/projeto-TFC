60dcd2cc33586c49296670cd31f669eb24ea9b6a
andremorsch <andremorsch37@gmail.com>
2022-03-24 18:40:57 -0300

    cria Controllers post /matchs

--- app/backend/src/database/controllers/Matchs.ts
+++ app/backend/src/database/controllers/Matchs.ts
@@ -7,6 +7,17 @@ const GetAll = async (_req: Request, res: Response) => {
   res.status(allMatchs.code).json(allMatchs.message);
 };
 
+const Create = async (req: Request, res: Response) => {
+  const { homeTeam, awayTeam, homeTeamGoals, awayTeamGoals, inProgress } = req.body;
+  const authorization = req.headers.authorization || '';
+
+  const matchObj = { homeTeam, awayTeam, homeTeamGoals, awayTeamGoals, inProgress };
+  const match = await MatchsServices.Create(matchObj, authorization);
+
+  res.status(match.code).json(match.message);
+};
+
 export default {
   GetAll,
+  Create,
 };
