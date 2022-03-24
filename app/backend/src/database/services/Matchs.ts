d6770bbeecf10579f26e1e2c1d26a57e308d83e4
andremorsch <andremorsch37@gmail.com>
2022-03-24 19:38:36 -0300

    refatora status code

--- app/backend/src/database/services/Matchs.ts
+++ app/backend/src/database/services/Matchs.ts
@@ -14,45 +14,51 @@ const prepareResponse = (
 });
 
 const secret = 'super_senha';
+const messageError1 = 'It is not possible to create a match with two equal teams';
+
+const existClub = async (homeTeam: number, awayTeam: number) => {
+  try {
+    const homeClub = await Clubs.findByPk(homeTeam);
+    const awayClub = await Clubs.findByPk(awayTeam);
+    if (!homeClub || !awayClub) {
+      return false;
+    }
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
 
 const GetAll = async () => {
-  try {
-    const allMatchs = await Matchs.findAll({
-      include: [
-        { model: Clubs, as: 'homeClub', attributes: { exclude: ['id'] } },
-        { model: Clubs, as: 'awayClub', attributes: { exclude: ['id'] } },
-      ],
-    });
+  const allMatchs = await Matchs.findAll({
+    include: [
+      { model: Clubs, as: 'homeClub', attributes: { exclude: ['id'] } },
+      { model: Clubs, as: 'awayClub', attributes: { exclude: ['id'] } },
+    ],
+  });
 
-    return prepareResponse(true, 200, allMatchs);
-  } catch (error) {
-    return prepareResponse(false, 666, { message: 'Catch Error' });
-  }
+  return prepareResponse(true, 200, allMatchs);
 };
 
 const Create = async (matchObj: IMatch, authorization: string) => {
   try {
-    const { homeTeam, awayTeam, homeTeamGoals, awayTeamGoals, inProgress } = matchObj;
+    const { homeTeam, awayTeam, inProgress } = matchObj;
     await Jwt.verify(authorization, secret);
-    await Clubs.findByPk(homeTeam);
-    await Clubs.findByPk(awayTeam);
-
-    console.log(homeTeam, awayTeam, homeTeamGoals, awayTeamGoals, inProgress);
-
-    if (homeTeam === awayTeam) {
-      return prepareResponse(false, 401, { message: 'Clubs must be different' });
+    if (await existClub(homeTeam, awayTeam) === false) {
+      return prepareResponse(false, 401, { message: 'There is no team with such id!' });
     }
-
     if (!inProgress) {
       return prepareResponse(false, 401, { message: 'Match must be in progress' });
+    }
+    if (homeTeam === awayTeam) {
+      return prepareResponse(false, 401, { message: messageError1 });
     }
 
     const match = await Matchs.create(matchObj);
 
     return prepareResponse(true, 200, match);
   } catch (error) {
-    console.log(error);
-    return prepareResponse(false, 401, { message: 'Catch Error - Token - Invalid Team' });
+    return prepareResponse(false, 401, { message: 'Invalid Token' });
   }
 };
 
